859e773a5b6c781fcd081cd67335491e651f4dc6
Vinicius <vinigugelmin@gmail.com>
2024-03-19 14:41:56 -0300

    feat: routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)},
+  {path: 'movies', loadComponent: () => import('./pages/movies/movies.component').then(m => m.MoviesComponent)},
+  {path: 'movie/:id', loadComponent: () => import('./pages/movie/movie.component').then(m => m.MovieComponent)},
+  {path: '**', loadComponent: () => import('./pages/not-found/not-found.component').then(m => m.NotFoundComponent)}
+];
